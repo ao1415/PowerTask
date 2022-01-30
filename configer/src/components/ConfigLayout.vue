ca32ff8574bf352dc4b2ec2aa78b318d453319e8
ao1415 <ao14159265@gmail.com>
2022-01-30 11:27:29 +0900

    概形を作成

---
+++ configer/src/components/ConfigLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="d-flex h-100">
+    <div class="h-100 p-1 border sidebar">
+      <div class="list-group">
+        <a
+          class="list-group-item list-group-item-action"
+          :class="{ active: selected == 'WinCron' }"
+          @click="selected = 'WinCron'"
+          ><i class="bi bi-list-task me-1"></i>WinCron</a
+        >
+      </div>
+    </div>
+    <div class="h-100 w-100">
+      <div v-if="selected == 'WinCron'" class="h-100"><WinCron /></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import WinCron from "./WinCron.vue";
+
+export default defineComponent({
+  name: "ConfigLayout",
+  components: {
+    WinCron,
+  },
+  setup() {
+    null;
+  },
+  data() {
+    return {
+      selected: "WinCron",
+    };
+  },
+});
+</script>
+
+<style scoped>
+.sidebar {
+  width: 15em;
+  min-width: 15em;
+}
+</style>
---
+++ configer/src/components/WinCron.vue
@@ -0,0 +1,248 @@
+<template>
+  <div class="d-flex flex-column h-100">
+    <ul class="list-group border list-region">
+      <li class="list-group-item list-group-item-action disabled">
+        <div class="row">
+          <div class="col-md-1 border-end">有効</div>
+          <div class="col-md-2 border-end">名前</div>
+          <div class="col-md-2 border-end">説明</div>
+          <div class="col-md-2 border-end">周期</div>
+          <div class="col-md border-end">パス</div>
+          <div class="col-md-2">引数</div>
+        </div>
+      </li>
+      <li
+        v-for="(item, index) in getConfigList"
+        :key="item"
+        class="list-group-item list-group-item-action"
+        :class="{ active: item.active }"
+        @click="onListClick(index)"
+      >
+        <div class="row">
+          <div class="col-md-1 border-end">{{ item.enable }}</div>
+          <div class="col-md-2 border-end">{{ item.name }}</div>
+          <div class="col-md-2 border-end">{{ item.explain }}</div>
+          <div class="col-md-2 border-end">{{ item.timing }}</div>
+          <div class="col-md border-end">{{ item.path }}</div>
+          <div class="col-md-2">{{ item.param }}</div>
+        </div>
+      </li>
+    </ul>
+    <div class="container pt-3 border form-region">
+      <div class="row g-3 mb-3">
+        <div class="col-md-auto">
+          <div class="form-check form-switch">
+            <input
+              type="checkbox"
+              id="input-enable"
+              class="form-check-input"
+              v-model="enable"
+            />
+            <label for="input-enable" class="form-label">有効</label>
+          </div>
+        </div>
+        <div class="ms-auto col-md-auto">
+          <button class="btn btn-primary">保存</button>
+        </div>
+      </div>
+      <div class="row g-3 mb-3">
+        <div class="col-md-5">
+          <label for="input-name" class="form-label">名前</label>
+          <input
+            type="text"
+            id="input-name"
+            class="form-control"
+            placeholder="名前"
+            v-model="name"
+            :readonly="!enable"
+          />
+        </div>
+        <div class="col-md-7">
+          <label for="input-explain" class="form-label">説明</label>
+          <input
+            type="text"
+            id="input-explain"
+            class="form-control"
+            placeholder="説明"
+            v-model="explain"
+            :readonly="!enable"
+          />
+        </div>
+      </div>
+      <div class="row g-3 mb-3">
+        <div class="col-md-4">
+          <label for="input-timing" class="form-label"
+            >周期設定（秒分時日月週）</label
+          >
+          <input
+            type="text"
+            id="input-timing"
+            class="form-control"
+            placeholder="* * * * * *"
+            v-model="timing"
+            :readonly="!enable"
+          />
+        </div>
+      </div>
+      <div class="row g-3 mb-3">
+        <div class="col-md-8">
+          <label for="input-path" class="form-label"
+            >スクリプトファイルパス</label
+          >
+          <input
+            type="text"
+            id="input-path"
+            class="form-control"
+            placeholder="ファイルパス"
+            v-model="path"
+            :readonly="!enable"
+          />
+        </div>
+        <div class="col-md-4">
+          <label for="input-param" class="form-label">スクリプト引数</label>
+          <input
+            type="text"
+            id="input-param"
+            class="form-control"
+            placeholder="引数"
+            v-model="param"
+            :readonly="!enable"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import fs from "fs";
+import path from "path";
+
+type ConfigType = {
+  enable: boolean;
+  name: string;
+  explain: string;
+  timing: string;
+  path: string;
+  param: string;
+  active: boolean;
+};
+type ConfigListType = ConfigType[];
+
+type ConfigJsonType = {
+  enable: boolean;
+  name: string;
+  explain: string;
+  timing: string;
+  path: string;
+  param: string;
+};
+type ConfigJsonListType = ConfigJsonType[];
+
+export default defineComponent({
+  setup() {
+    null;
+  },
+  data(): {
+    enable: boolean;
+    name: string;
+    explain: string;
+    timing: string;
+    path: string;
+    param: string;
+    configList: ConfigListType;
+  } {
+    return {
+      enable: false,
+      name: "",
+      explain: "",
+      timing: "",
+      path: "",
+      param: "",
+      configList: [],
+    };
+  },
+  created() {
+    this.onLoadConfig();
+  },
+  computed: {
+    isEnable(): boolean {
+      return this.enable;
+    },
+    getConfigList(): ConfigListType {
+      return this.configList;
+    },
+  },
+  methods: {
+    onListClick(index: number) {
+      this.configList.forEach((element) => {
+        element.active = false;
+      });
+      this.configList[index].active = true;
+
+      this.enable = this.configList[index].enable;
+      this.name = this.configList[index].name;
+      this.explain = this.configList[index].explain;
+      this.timing = this.configList[index].timing;
+      this.path = this.configList[index].path;
+      this.param = this.configList[index].param;
+    },
+    onSaveClick() {
+      const filePath = path.resolve("./WinCron.json");
+      fs.writeFileSync(filePath, "test");
+    },
+    onLoadConfig(): void {
+      try {
+        const filePath = path.resolve("/Config/WinCron.json");
+        console.debug(filePath);
+        const text = fs.readFileSync(filePath, {
+          encoding: "utf8",
+        });
+        const jsonData: ConfigJsonListType = JSON.parse(text);
+
+        let configData: ConfigListType = [];
+
+        jsonData.forEach((element: ConfigJsonType) => {
+          configData.push({
+            enable: element.enable,
+            name: element.name,
+            explain: element.explain,
+            timing: element.timing,
+            path: element.path,
+            param: element.param,
+            active: false,
+          });
+        });
+        // 新規追加用の空設定
+        configData.push({
+          enable: false,
+          name: "",
+          explain: "",
+          timing: "",
+          path: "",
+          param: "",
+          active: false,
+        });
+
+        this.configList = configData;
+      } catch (error) {
+        console.error("ファイル読み込み失敗", error);
+      }
+    },
+  },
+});
+</script>
+
+<style scoped>
+.list-region {
+  overflow-y: scroll;
+  min-height: 7em;
+  height: calc(100% - 22em);
+}
+.form-region {
+  overflow-y: scroll;
+  min-height: 22em;
+  max-height: 22em;
+}
+</style>
